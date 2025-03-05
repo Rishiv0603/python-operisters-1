4e4c7c1a0d81f94cfc92519d724f8b6fbfc04905
Rishiv0603 <sarataus2020@gmail.com>
2025-03-05 19:49:38 +1100

    percentage

---
+++ perscent.py
@@ -0,0 +1,7 @@
+math=int( input('enter your maths marks'))
+science=int(input('enter your SCIENCE MARKS'))
+english=int( input('enter your english marks'))
+sum=math+science+english
+print(sum)
+percentage=( sum/300)*100
+print( percentage)
